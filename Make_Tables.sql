f0744baf249a556b0918044c10a205a942488a37
jtorre39 <jtorre39@lion.lmu.edu>
2017-11-13 23:11:21 -0800

    added script to make tables

---
+++ Make_Tables.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Market (
+    market_Index serial NOT NULL,
+    address char NOT NULL,
+    name char NOT NULL,
+    business_hours char NOT NULL,
+    minutes_by_walk double precision NOT NULL,
+    minutes_by_car double precision NOT NULL,
+    minutes_by_bike double precision NOT NULL,
+    miles_from_lmu double precision NOT NULL,
+    PRIMARY KEY (market_index)
+);
+
+CREATE TABLE Food (
+    name char NOT NULL,
+    food_category char NOT NULL,
+    food_index serial NOT NULL,
+    is_organic boolean NOT NULL,
+    is_vegetarian boolean NOT NULL,
+    is_vegan boolean NOT NULL,
+    brand char NOT NULL
+    PRIMARY KEY (food_index)
+);
+
+CREATE TABLE Sells (
+    food_index char references Market(food_index),
+    food_index char references Market(market_index),
+    sells_index serial NOT NULL,
+    PRIMARY KEY (sells_index)
+);
