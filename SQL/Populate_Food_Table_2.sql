c6479a76b624aae8028c788cfebf24afdcd98090
jtorre39 <jtorre39@lion.lmu.edu>
2017-11-27 17:19:18 -0800

    finalized queries and sql scripts

---
+++ SQL/Populate_Food_Table_2.sql
@@ -0,0 +1,9 @@
+INSERT INTO Food (name, food_category, is_organic, is_vegan, is_vegetarian, brand, price, is_lbs, market_index)
+VALUES
+    ('Oreo Cheesecake', 'pastry', TRUE, FALSE, TRUE, NULL, 4.65, FALSE, 1),
+    ('Jumbo Organic Brown Egg', 'egg', TRUE, FALSE, TRUE, NULL, 3.25, FALSE, 1),
+    ('Jumbo Egg', 'egg', TRUE, FALSE, TRUE, NULL, 2.89, FALSE, 2),
+    ('Organic Black Beans', 'canned', TRUE, TRUE, TRUE, NULL, 2.99, FALSE, 2),
+    ('Organic Tomatoes', 'vegetable', TRUE, TRUE, TRUE, NULL, 2.57, TRUE, 2),
+    ('Organic Apple Sauce', 'snack', TRUE, TRUE, TRUE, 'Dole', 3.15, FALSE, 2),
+    ('Fruit Tart', 'pastry', FALSE, FALSE, TRUE, 'Ralph''s', 19.99, FALSE, 2);
